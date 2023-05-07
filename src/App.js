1cbefe4e34c52688b9e8abd01ab0feb17509dc02
V1P3RS <86308839+V1P3RS@users.noreply.github.com>
2023-05-08 03:48:55 +0530

    adding AOS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import MiscNotes from "./components/MiscNotes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 1000,
+  offset: 200,
+});
 
 const App = () => {
   return (
-    <>
+    <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
       <MiscNotes />
-    </>
+    </div>
   );
 };
 
